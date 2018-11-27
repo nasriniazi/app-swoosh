4485d0a7cbdb5207f610107f24d72eeffb34e35b
Niazi <niazi@Niazis-MacBook-Pro.local>
2018-11-27 12:14:30 +0330

    beginner autolayout

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -21,8 +21,8 @@ class ViewController: UIViewController {
         print((view.frame.size.width / 2) - (swoosh.frame.size.width / 2))
 
 //       swoosh.frame = CGRect(x: ((view.frame.size.width / 2) - (swoosh.frame.size.width / 2)), y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
-        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
-bgImg.frame = view.frame
+//        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+//bgImg.frame = view.frame
     }
 
     override func didReceiveMemoryWarning() {
